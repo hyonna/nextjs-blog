f78223ccc63cb37c926b4d0d86f96c7dfbe1e7c2
윤현아 <hyonna@yunhyeon-aui-MacBookPro.local>
2022-12-03 20:48:29 +0900

    initial commit

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,28 @@
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Layout from "../../components/layout";
+import { useEffect } from "react";
 
 export default function FirstPost() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/posts/first-post/?counter=10", undefined, { shallow: true });
+  }, []);
+
+  useEffect(() => {
+    alert(router.query.counter);
+  }, [router.query.counter]);
+
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
-        <Link href="/">Back to home</Link>
+        <Link href="/">← Back to home</Link>
       </h2>
-    </>
-  )
+    </Layout>
+  );
 }
